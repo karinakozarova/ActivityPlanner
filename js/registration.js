aad9863f02eba3bdb0ec42c17a59a153b26ce938
karinakozarova <karinakozarova1303@gmail.com>
2020-02-17 14:53:15 +0100

    added javascript for validation of the form

---
+++ js/registration.js
@@ -0,0 +1,49 @@
+function vizualiseError(elementId) {
+    let element = document.getElementById(elementId);
+    element.classList.remove("hidden");
+}
+
+function hideError(elementId) {
+    let element = document.getElementById(elementId);
+    element.classList.add("hidden");
+}
+
+function validateEmail(email) {
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateForm() {
+    // check all fields are populated
+    let inputs, index;
+    inputs = document.getElementsByTagName('input');
+    for (index = 0; index < inputs.length; ++index) {
+        if (inputs[index].value === "") {
+            vizualiseError('populatedFieldsError');
+            return false;
+        }
+    }
+    hideError('populatedFieldsError');
+
+    // check passwords match
+    let password1 = document.getElementById('password');
+    let password2 = document.getElementById('confirmPassword');
+
+    if (password1.value !== password2.value) {
+        vizualiseError('passwordsMatchError');
+        return false;
+    } else{
+        hideError('passwordsMatchError');
+    }
+
+    let email = document.getElementById('email');
+    // check email is valid
+    if(!validateEmail(email)){
+        vizualiseError('validMailError');
+        return false;
+    } else{
+        hideError('validMailError');
+    }
+
+    return true;
+}
