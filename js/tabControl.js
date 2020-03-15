989edaea58fda263d0580993c5182c088d58563a
Karina Kozarova <karinakozarova1303@gmail.com>
2020-03-15 23:20:43 +0100

    started js logic for tabs

---
+++ js/tabControl.js
@@ -0,0 +1,6 @@
+function hideAll(){
+    document.getElementById("tab-overview").classList.add("hidden");
+    document.getElementById("tab-planner").classList.add("hidden");
+    document.getElementById("tab-achievements").classList.add("hidden");
+    document.getElementById("tab-goals").classList.add("hidden");
+}
