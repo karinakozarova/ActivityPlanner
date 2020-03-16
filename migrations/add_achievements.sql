c9bc34e9a5b5c0af2cc88bbf946fcc287dcc9443
Karina Kozarova <karinakozarova1303@gmail.com>
2020-03-16 15:58:24 +0100

    fix migration

--- migrations/add_achievements.sql
+++ migrations/add_achievements.sql
@@ -1,6 +1,6 @@
 CREATE TABLE achievements (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name INT NOT NULL ,
+    name VARCHAR(100) NOT NULL ,
     user_id INT NOT NULL ,
     description VARCHAR(300),
     received_on DATE NOT NULL
