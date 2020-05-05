31f54e8c182de76d9a40476090e529006f644840
Karina Kozarova <karinakozarova1303@gmail.com>
2020-05-05 22:40:17 +0200

    refactor the html and export the css

--- html/editAchievement.php
+++ html/editAchievement.php
@@ -43,7 +43,7 @@ $achievement = Achievement::getUserAchievementById($_REQUEST['id']);
         <div class="profile-nav">
             <ul>
                 <li>
-                    <a onclick='showOverview();'>
+                    <a href="../html/profile-dashboard.php">
                         <div class="nav-item active" id="overview-nav">
                             <svg width="30" height="30" viewBox="0 0 24 24">
                                 <path d="M21.698 10.658l2.302 1.342-12.002 7-11.998-7 2.301-1.342 9.697 5.658 9.7-5.658zm-9.7 10.657l-9.697-5.658-2.301 1.343 11.998 7 12.002-7-2.302-1.342-9.7 5.657zm12.002-14.315l-12.002-7-11.998 7 11.998 7 12.002-7z"/>
--- html/tab-achivements.inc.php
+++ html/tab-achivements.inc.php
@@ -26,15 +26,12 @@ $achievements = $achievements->getUserAchievements($_SESSION['userid']);
         foreach ($achievements as $achievement) { ?>
             <div class="column">
                 <div class="card">
-                    <img src="../images/achievement.jpg" alt="achievement"
-                         style="width: 100%;height: 100%">
-
+                    <img src="../images/achievement.jpg" alt="achievement" class="achievement-picture">
                     <div class="container">
-                        <h4><b><?= $achievement->name; ?> received
-                                on <?= $achievement->receivedOn ?></b>
+                        <h4><b><?= $achievement->name; ?> received on <?= $achievement->receivedOn ?></b>
                         </h4>
                         <p><?= $achievement->description; ?></p>
-                        <button class="btn-light" style="margin-left: 55%;" value="<?= $achievement->id?>" onclick="editAchievement($(this).val());">
+                        <button class="btn-light left-margin" value="<?= $achievement->id?>" onclick="editAchievement($(this).val());">
                             <i class="fa fa-edit"></i>
                         </button>
                         <button class="btn-light" value="<?= $achievement->id ?>"
@@ -53,34 +50,7 @@ $achievements = $achievements->getUserAchievements($_SESSION['userid']);
 
 <script>
     var achievements = <?= json_encode($achievements) ?>;
-    let achievementsB64 = btoa(JSON.stringify(achievements));
-
-    var downloadLink = "data:application/octet-stream;charset=utf-8;base64," + achievementsB64;
-    document.getElementById("downloadAchievementsBttn").setAttribute("href", downloadLink);
-
-    function deleteAchievement(id) {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.value) {
-                Swal.fire(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
-                )
-                window.location.href = '../backend/deleteAchievement.php?id=' + id;
-            }
-        })
-    }
 </script>
-<script>
-    function editAchievement(id) {
-        window.location.href = '../html/editAchievement.php?id=' + id;
-    }
-</script>
+<script src="../js/downloadAchievement.js"></script>
+<script src="../js/deleteAchievements.js"></script>
+<script src="../js/editAchievement.js"></script>
