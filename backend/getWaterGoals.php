f15f8258cc5c1564fe320caa482aaece6ce38fb8
Karina Kozarova <karinakozarova1303@gmail.com>
2020-05-05 11:21:21 +0200

    started refactoring

--- backend/getWaterGoals.php
+++ backend/getWaterGoals.php
@@ -1,13 +1,6 @@
 <?php
-require_once("../configurations/credentials.php");
+require_once("../backend/waterGoals.class.php");
 date_default_timezone_set('Europe/Amsterdam');
 
-$userId = $_SESSION['userid'];
-
-$conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$query = $conn->prepare("SELECT cups FROM water_goals WHERE user_id=\"$userId\" ORDER BY id desc LIMIT 1");
-$query->execute();
-$element = $query->fetch();
-
+$element = WaterGoals::getWaterGoals($_SESSION['userid']);
 $waterCupsGoals = $element['cups'] == NULL ? 0 : $element['cups'];
---
+++ backend/waterIntake.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Class WaterGoals
+ */
+class WaterIntake
+{
+    /**
+     * @param $userId
+     * @return mixed
+     */
+    public static function getWaterIntake($userId)
+    {
+        require("../configurations/credentials.php");
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $query = $conn->prepare("SELECT cups FROM water_goals WHERE user_id=\"$userId\" ORDER BY id desc LIMIT 1");
+        $query->execute();
+        return $query->fetch();
+    }
+
+    /**
+     * @param int $userId
+     * @param int $quantity
+     */
+    public static function addWaterGoals(int $userId, int $quantity = 0): void
+    {
+        require("../configurations/credentials.php");
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $query = $conn->prepare("INSERT INTO water_goals(user_id, cups) VALUES (\"$userId\",\"$quantity\")");
+        $query->execute();
+    }
+}
+
